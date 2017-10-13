9d964b75aa233d76ae99cbd31abe370de7f666aa
Brian Freeman <brian.freeman@darkmatter2bd.com>
2017-10-13 11:30:56 -0400

    OnEventStream

---
+++ CSharp/OnEventStream.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.IO;
+
+namespace DM2BD.DTK.DAL.Lib
+{
+    /// <summary>
+    /// Wraps another stream and provides reporting for when bytes are read or written to the stream.
+    /// </summary>
+    public class OnEventStream : Stream
+    {
+        #region Private Data Members
+        private Stream innerStream;
+        private Action<OnEventStream> _onclosed_action;
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// Creates a new Stream supplying the stream for it to report on.
+        /// </summary>
+        /// <param name="streamToReportOn">The underlying stream that will be reported on when bytes are read or written.</param>
+        public OnEventStream(Stream streamToReportOn, Action<OnEventStream> OnClosed =null)
+        {
+            if (streamToReportOn != null)
+            {
+                this.innerStream = streamToReportOn;
+            }
+            else
+            {
+                throw new ArgumentNullException("streamToReportOn");
+            }
+            _onclosed_action = OnClosed;
+
+        }
+        #endregion
+
+
+
+        #region Stream Members
+
+        public override bool CanRead
+        {
+            get { return innerStream.CanRead; }
+        }
+
+        public override bool CanSeek
+        {
+            get { return innerStream.CanSeek; }
+        }
+
+        public override bool CanWrite
+        {
+            get { return innerStream.CanWrite; }
+        }
+
+        public override void Flush()
+        {
+            innerStream.Flush();
+        }
+
+        public override long Length
+        {
+            get { return innerStream.Length; }
+        }
+
+        public override long Position
+        {
+            get
+            {
+                return innerStream.Position;
+            }
+            set
+            {
+                innerStream.Position = value;
+            }
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            var bytesRead = innerStream.Read(buffer, offset, count);
+            return bytesRead;
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            return innerStream.Seek(offset, origin);
+        }
+
+        public override void SetLength(long value)
+        {
+            innerStream.SetLength(value);
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            innerStream.Write(buffer, offset, count);
+        }
+
+        public override void Close()
+        {
+            innerStream.Close();
+            base.Close();
+
+            if (_onclosed_action != null)
+            {
+                _onclosed_action.Invoke(this);
+            }
+        }
+        #endregion
+    }
+
+
+
+
+
+
+
+
+}
+
---
+++ CSharp/OnEventStream_Example.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.SqlClient;
+using System.Linq;
+
+
+    public class WorkflowAttachment
+    {
+        private static readonly global::Common.Logging.ILog Log = global::Common.Logging.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public static async System.Threading.Tasks.Task<System.IO.Stream> GetWorkflowAttachmentData(int attachmentid)
+        {
+
+
+                //... Setup SqlCommand 1st return Blob or SQLFileStream
+
+                // The reader needs to be executed with the SequentialAccess behavior to enable network streaming
+                // Otherwise ReadAsync will buffer the entire BLOB into memory which can cause scalability issues or even OutOfMemoryExceptions
+                SqlDataReader reader = await cmd.ExecuteReaderAsync(System.Data.CommandBehavior.SequentialAccess);
+
+                if (await reader.ReadAsync())
+                {
+                    if (!(await reader.IsDBNullAsync(0)))
+                    {
+                        return new Lib.OnEventStream(reader.GetStream(0),
+                            OnClosed: (Lib.OnEventStream stream) =>
+                            {
+                                Log.Debug("Stream Close; Disposing DB items.");
+                                reader.Dispose();
+                                cmd.Dispose();
+                                db.Dispose();
+                            }
+                            );
+                    }
+                }
+            }
+            throw new ApplicationException("GetWorkflowAttachmentData not there.");
+        }
+
+
+    }
