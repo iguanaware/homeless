8c087ae67428acede48ff23096007e3d727eb05f
Brian Freeman <brian.freeman@darkmatter2bd.com>
2018-06-28 15:05:45 -0400

    Added Poly memoryCache Example

---
+++ CSharp/PollyExample/CacheableResults.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace PollyExample
+{
+    public class CacheableResults<TResult>
+    {
+        [DebuggerDisplay("{Item}")]
+        private class Wrapper<T>
+        {
+            public Wrapper(T item)
+            {
+                this.Item = item;
+            }
+            public T Item { get; set; }
+
+            public override string ToString()
+            {
+                return this.Item.ToString();
+            }
+        }
+
+        private readonly Microsoft.Extensions.Caching.Memory.IMemoryCache memoryCache;
+        private readonly Polly.Caching.Memory.MemoryCacheProvider memoryCacheProvider;
+        private readonly Polly.Caching.CachePolicy cachePolicy;
+        private readonly Polly.Caching.CachePolicy cachePolicyAsync;
+        private readonly Polly.Caching.Ttl _dummy_ttl = new Polly.Caching.Ttl(TimeSpan.FromSeconds(1));
+        private readonly System.Threading.CancellationToken _dummy_cancellationToken = new System.Threading.CancellationToken();
+        public CacheableResults(TimeSpan ts = default(TimeSpan))
+        {
+            if (ts == default(TimeSpan))
+            {
+                ts = TimeSpan.FromMinutes(15);
+            }
+            memoryCache = new Microsoft.Extensions.Caching.Memory.MemoryCache(new Microsoft.Extensions.Caching.Memory.MemoryCacheOptions() { });
+            memoryCacheProvider = new Polly.Caching.Memory.MemoryCacheProvider(memoryCache);
+            cachePolicy = Polly.Policy.Cache(memoryCacheProvider, ts);
+            cachePolicyAsync = Polly.Policy.CacheAsync(memoryCacheProvider, ts);
+        }
+
+        [DebuggerStepThrough]
+        public async Task<TResult> ExecuteAsync(Func<System.Threading.CancellationToken, Task<TResult>> function, string key, System.Threading.CancellationToken cancellationToken)
+        {
+            Func<Polly.Context, System.Threading.CancellationToken, Task<Wrapper<TResult>>> revisedfunction = async (context, _cancellationToken) =>
+           {
+               TResult result = await function(_cancellationToken);
+               return new Wrapper<TResult>(result);
+           };
+            Wrapper<TResult> r = await cachePolicyAsync.ExecuteAsync<Wrapper<TResult>>(revisedfunction, new Polly.Context(key), cancellationToken);
+            return r.Item;
+        }
+
+        [DebuggerStepThrough]
+        public async Task<TResult> ExecuteAsync(Func<Task<TResult>> function, string key)
+        {
+            Func<System.Threading.CancellationToken, Task<TResult>> revisedfunction = async (a) =>
+            {
+                return await function();
+            };
+            return await this.ExecuteAsync(revisedfunction, key, _dummy_cancellationToken);
+        }
+
+        [DebuggerStepThrough]
+        public TResult Execute(Func<System.Threading.CancellationToken, TResult> function, string key, System.Threading.CancellationToken cancellationToken)
+        {
+            Func<Polly.Context, System.Threading.CancellationToken, Wrapper<TResult>> revisedfunction = (context, _cancellationToken) =>
+            {
+                TResult result = function(_cancellationToken);
+                return new Wrapper<TResult>(result);
+            };
+            Wrapper<TResult> r = cachePolicy.Execute<Wrapper<TResult>>(revisedfunction, new Polly.Context(key), cancellationToken);
+            return r.Item;
+        }
+        [DebuggerStepThrough]
+        public TResult Execute(Func<TResult> function, string key)
+        {
+            Func<System.Threading.CancellationToken, TResult> revisedfunction = (cancellationToken) => { return function(); };
+            return this.Execute(revisedfunction, key, _dummy_cancellationToken);
+        }
+        [DebuggerStepThrough]
+        public void Clear(string key)
+        {
+            memoryCacheProvider.Put(key, null, _dummy_ttl);
+        }
+        [DebuggerStepThrough]
+        public async void ClearAsync(string key, System.Threading.CancellationToken cancellationToken)
+        {
+            await memoryCacheProvider.PutAsync(key, null, _dummy_ttl, cancellationToken, false);
+        }
+
+
+    }
+}
---
+++ CSharp/PollyExample/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PollyExample
+{
+    class Program
+    {
+        static int global_dummy_value = -2;
+
+        static void Main(string[] args)
+        {
+            //Sync Example
+            var anystingkey = "same for demo";
+            var cr = new CacheableResults<int>();
+            for (int i = 1; i < 20; i++)
+            {
+                var r = cr.Execute(() => { System.Threading.Thread.Sleep(1000); Console.WriteLine("in"); return global_dummy_value++; }, anystingkey);
+                Console.WriteLine(r);
+                if (i % 3 == 0) cr.Clear(anystingkey);
+            }
+
+            //Async Example
+            var cr_async = new CacheableResults<string>();
+            for (int i = 1; i < 20; i++)
+            {
+                var rtask = cr_async.ExecuteAsync(async()  => { await Task.Delay(1000); Console.WriteLine("in"); return "cheese"; }, anystingkey);
+                rtask.Wait();
+                var r = rtask.Result;
+                Console.WriteLine(r);
+                if (i % 3 == 0) cr_async.Clear(anystingkey);
+            }
+        }
+
+
+    }
+}
